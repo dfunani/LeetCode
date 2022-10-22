68e60da1ca9189e356b5972d9983c80e4be24670
DelaliFunani <dfunani@live.co.za>
2022-10-22 23:54:47 +0200

    Time: 98 ms (43.20%), Space: 13.9 MB (30.72%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -13,10 +13,7 @@ class Solution:
         r = 0
         for i,c in enumerate(s):
             try:
-                if  (c == 'I' and s[i + 1] in ["V", "X"]) or (c == 'X' and s[i + 1] in ["L", "C"]) or (c == 'C' and s[i + 1] in ["D", "M"]):
-                    r -= conversion[c]
-                else:
-                    r  += conversion[c]
+                r += (-conversion[c]) if  (c == 'I' and s[i + 1] in ["V", "X"]) or (c == 'X' and s[i + 1] in ["L", "C"]) or (c == 'C' and s[i + 1] in ["D", "M"]) else conversion[c]
             except:
                 r += conversion[c]
         return r
